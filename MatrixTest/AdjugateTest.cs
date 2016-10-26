ef6ad21f09321823598dcada9284605e53f2a2c5
Sean <sean>
2016-10-25 23:48:24 -0700

    Add complex tests for Adjugate, Determinant, and Inverse proofs (simple proofs, complex methods)

---
+++ MatrixTest/AdjugateTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixTest
+{
+    [TestClass]
+    public class AdjugateTest
+    {
+        [TestMethod]
+        public void TestIdentity()
+        {
+            // adj(I) = I
+
+            var I3 = MatrixHelper.GetIdentityMatrix(3);
+            var I3Adj = I3.Adjoint();
+
+            Assert.AreEqual(I3, I3Adj);
+        }
+
+        [TestMethod]
+        public void TestTransitiveProduct()
+        {
+            // adj(AB) = ( adj(B) )( adj(A) )
+
+            var A = MatrixHelper.GetSquareMatrix(3);
+            var B = MatrixHelper.GetSquareMatrix(3, -50, 50);
+
+            var AB = A * B;
+            var ABAdj = AB.Adjoint();
+
+            var BAdj = B.Adjoint();
+            var AAdj = A.Adjoint();
+
+            // BAdjAAdjABABJjajdjJABj
+            var BAdjAAdj = BAdj * AAdj;
+
+            Assert.AreEqual(ABAdj, BAdjAAdj);
+        }
+
+        [TestMethod]
+        public void TestTransitiveScalar()
+        {
+            // adj(cA) = ( c^(n - 1) )( adj(A) )
+
+            int _n = 3;
+
+            double _const = 5;
+            var A = MatrixHelper.GetSquareMatrix(_n);
+
+            var cA = A * _const;
+            var cAAdj = cA.Adjoint(); // adj(cA)
+
+            var cNInverse = Math.Pow(_const, _n - 1);
+            var AAdj = A.Adjoint();
+
+            var cNAAdj = AAdj * cNInverse; // ( c^(n - 1) )( adj(A) )
+
+            Assert.AreEqual(cAAdj, cNAAdj);
+        }
+    }
+}
---
+++ MatrixTest/DeterminantTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MatrixAreForKids;
+
+namespace MatrixTest
+{
+    [TestClass]
+    public class DeterminantTest
+    {
+        [TestMethod]
+        public void TestTransitiveScalar()
+        {
+            // det(cA) = det(A) * c^n
+
+            int _n = 3;
+
+            double _const = 5;
+            var A = MatrixHelper.GetSquareMatrix(_n);
+
+            var cA = A * _const; 
+            var cADet = cA.Determinant(); // det(cA)
+
+            var ADet = A.Determinant();
+            var cN = Math.Pow(_const, _n);
+
+            var cNADet = ADet * cN; // det(A) * c^n
+
+            Assert.AreEqual(cADet, cNADet);
+            
+        }
+    }
+}
